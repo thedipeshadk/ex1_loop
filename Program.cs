f3151c44a3eb4118e532e12a9bf8b05dccd6a3c3
Dipesh <dipesh333adhikari@gmail.com>
2024-02-13 20:52:59 -0500

    final commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex1_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            bool isValid;
+
+            do
+            {
+                Console.Write("Enter a number below 10: ");
+                string input = Console.ReadLine();
+                isValid = int.TryParse(input, out num) && num < 10;
+
+                if (!isValid)
+                {
+                    Console.WriteLine("Invalid input. Please enter a number below 10.");
+                }
+            } while (!isValid);
+
+            Console.WriteLine($"Times table for {num}:");
+
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.WriteLine($"{i} * {num} = {i * num}");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
